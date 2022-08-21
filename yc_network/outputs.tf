4fd9fee6445e9cfd5896f44345a67e0b90f30927
Vladimir <cAB-8:VF>
2022-08-21 12:28:19 +0300

    refactor

--- yc_network/outputs.tf
+++ yc_network/outputs.tf
@@ -16,16 +16,16 @@ output "external_ips" {
   value = yandex_vpc_address.ext_ip[*].id
 }
 output "public_subnets" {
-  value = { for v in yandex_vpc_subnet.public_subnets : v.zone => map(
+  value = { for v in yandex_vpc_subnet.public_subnets : v.zone => tomap({
     "id", v.id,
     "name", v.name,
     "zone", v.zone
-  ) }
+  }) }
 }
 output "private_subnets" {
-  value = { for v in yandex_vpc_subnet.private_subnets : v.zone => map(
+  value = { for v in yandex_vpc_subnet.private_subnets : v.zone => tomap({
     "id", v.id,
     "name", v.name,
     "zone", v.zone
-  ) }
+  }) }
 }
