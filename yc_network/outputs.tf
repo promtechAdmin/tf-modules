32fc21716741c75db4762550d71088aee7de3353
Vladimir <cAB-8:VF>
2022-08-21 12:25:15 +0300

    refactor

--- yc_network/outputs.tf
+++ yc_network/outputs.tf
@@ -1,6 +1,6 @@
-output "folder_id" {
-  value = yandex_resourcemanager_folder.folder.id
-}
+#output "folder_id" {
+#  value = yandex_resourcemanager_folder.folder.id
+#}
 output "vpc_id" {
   value = yandex_vpc_network.main.id
 }
