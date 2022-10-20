14d6f0d0ad281a79260991271a3b3f296ec621e5
Zakharovzvv <zakharovzvv@gmail.com>
2022-10-20 12:36:53 +0300

    fixed variables yc pg managed claster

--- yc_mdb_postgresql_cluster/variables.tf
+++ yc_mdb_postgresql_cluster/variables.tf
@@ -48,22 +48,6 @@ variable "ver_pg" {
   type = string
   default = "14C"
 }
-variable "pg_db_name" {
-  description = "PostgeSQL cluster and database name"
-  type = string
-  default = "keycloak"
-}
-
-variable "pg_db_user" {
-  description = "PostgeSQL database user name"
-  type = string
-}
-
-variable "pg_db_pass" {
-  description = "PostgeSQL database user's password"
-  type = string
-}
-
 variable "disk_size" {
   type    = number
   default = 10
