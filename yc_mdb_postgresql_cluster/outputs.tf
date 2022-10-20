722f91e8e5cb83942d0ffd6ac50a7c5706a217fb
Zakharovzvv <zakharovzvv@gmail.com>
2022-10-20 14:51:20 +0300

    added user & db

--- yc_mdb_postgresql_cluster/outputs.tf
+++ yc_mdb_postgresql_cluster/outputs.tf
@@ -4,3 +4,9 @@ output "mdb_postgresql_cluster_id" {
 output "mdb_postgresql_cluster_host_fqdn" {
   value=yandex_mdb_postgresql_cluster.pg_cluster.host.*.fqdn
 }
+output "mdb_postgresql_cluster_user" {
+  value=yandex_mdb_postgresql_user.pg_user
+}
+output "mdb_postgresql_cluster_db" {
+  value=yandex_mdb_postgresql_database.pg_db.*
+}
