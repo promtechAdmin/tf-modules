dc32ee8114b17a0181d371c67bacb58d779c02a3
Zakharovzvv <zakharovzvv@gmail.com>
2022-10-20 13:12:10 +0300

    fixed output vars

--- yc_mdb_postgresql_cluster/outputs.tf
+++ yc_mdb_postgresql_cluster/outputs.tf
@@ -1,3 +1,6 @@
 output "mdb_postgresql_cluster_id" {
   value=yandex_mdb_postgresql_cluster.pg_cluster.id
 }
+output "mdb_postgresql_cluster_host_fqdn" {
+  value=yandex_mdb_postgresql_cluster.pg_cluster.host.*.fqdn
+}
