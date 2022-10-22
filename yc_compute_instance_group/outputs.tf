8a8ff44038c225a965327d644b34189357cebea2
Zakharovzvv <zakharovzvv@gmail.com>
2022-10-22 20:04:28 +0300

    added compute instance group

--- yc_compute_instance_group/outputs.tf
+++ yc_compute_instance_group/outputs.tf
@@ -1,15 +1,4 @@
-output "instance_name" {
-  value=yandex_compute_instance.server.*.fqdn
+output "compute_instance_group_id" {
+  value=yandex_compute_instance_group.instance_group.id
 }
-output "instance_internal_ip" {
-  value=yandex_compute_instance.server.*.network_interface.0.ip_address
-}
-output "instance_external_ip" {
-  value=yandex_compute_instance.server.*.network_interface.0.nat_ip_address
-}
-output "instance_labels" {
-  value=yandex_compute_instance.server.*.labels
-}
-output "secondary_disks" {
-  value=yandex_compute_disk.secondary_disk.*.id
-}
+
