68b94bd4110932afddcd371e9ba43df2796da495
Zakharovzvv <zakharovzvv@gmail.com>
2022-10-24 09:07:08 +0300

    added default values for variables

--- yc_compute_instance_group/outputs.tf
+++ yc_compute_instance_group/outputs.tf
@@ -5,6 +5,6 @@ output "compute_instance_group_id" {
 output "compute_instance_group_instances" {
   value = {
   for instance in yandex_compute_instance_group.instance_group.instances :
-  instance.id => instance.ip_address
+  instance.name => instance.ip_address
   }
 }
