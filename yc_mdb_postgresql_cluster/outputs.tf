06b024070fdf4cf7b4bcd233a9b88b40484e9d1e
Zakharovzvv <zakharovzvv@gmail.com>
2022-11-01 09:09:10 +0300

    fixed instance group

--- yc_mdb_postgresql_cluster/outputs.tf
+++ yc_mdb_postgresql_cluster/outputs.tf
@@ -2,7 +2,7 @@ output "mdb_postgresql_cluster_id" {
   value=yandex_mdb_postgresql_cluster.pg_cluster.id
 }
 output "mdb_postgresql_cluster_host_fqdn" {
-  value=yandex_mdb_postgresql_cluster.pg_cluster.host.*.fqdn
+  value=yandex_mdb_postgresql_cluster.pg_cluster.host.0.fqdn
 }
 output "mdb_postgresql_cluster_user" {
   value=yandex_mdb_postgresql_user.pg_user
