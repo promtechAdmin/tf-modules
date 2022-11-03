e335d9032fcf9789251bfce92dda195d355252a9
Zakharovzvv <zakharovzvv@gmail.com>
2022-11-03 12:49:40 +0300

    changed output vars

--- yc_compute_instance_group/outputs.tf
+++ yc_compute_instance_group/outputs.tf
@@ -2,12 +2,15 @@ output "compute_instance_group_id" {
   value = yandex_compute_instance_group.instance_group.id
 }
 
-output "compute_instance_group_instances" {
+output "compute_instance_group_instances_name_ip" {
   value = {
   for instance in yandex_compute_instance_group.instance_group.instances :
-  instance.name => instance.network_interface.0.ip_address
+    instance.name => instance.network_interface.0.ip_address
   }
 }
 output "load_balancer_target_group_id" {
   value = yandex_compute_instance_group.instance_group.load_balancer.*.target_group_id
+}
+output "compute_instance_group_instances" {
+  value = yandex_compute_instance_group.instance_group.instances
 }
